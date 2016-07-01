916f3b40132e996fb4bed2d2aa1cb025c2842d72
Hank Preston <hank.preston@gmail.com>
2016-07-01 09:41:27 -0400

    Clean up lab default app.

--- demoapp.py
+++ demoapp.py
@@ -11,14 +11,7 @@ class HelloWorld(Resource):
         text = "Hello World!"
         return text
 
-class HelloClass(Resource):
-    def get(self):
-        text = "Hello Class!"
-        return text
-
-
 api.add_resource(HelloWorld, '/hello/world')
-api.add_resource(HelloClass, '/hello/class')
 
 if __name__ == '__main__':
     # Runn Flask
