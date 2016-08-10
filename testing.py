809ac6630f77e7c87049fbfe1cf9d4574339bfff
kecorbin <kecorbin@cisco.com>
2016-08-10 15:10:46 -0500

    added testing

---
+++ testing.py
@@ -0,0 +1,23 @@
+import demoapp
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        demoapp.app.config['TESTING'] = True
+        self.app = demoapp.app.test_client()
+
+    def test_correct_http_response(self):
+        resp = self.app.get('/hello/world')
+        self.assertEquals(resp.status_code, 200)
+
+    def test_correct_content(self):
+        resp = self.app.get('/hello/world')
+        self.assertEquals(resp.data, '"Hello World!"\n')
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
